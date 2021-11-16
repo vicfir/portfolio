92b91082ff3b6ac261af508914c6077783a7d870
vicfir <vicfir@hotmail.fr>
2021-11-16 19:09:39 +0100

    tuto + amelioration liens,home,galerie

--- import/liens.php
+++ import/liens.php
@@ -13,10 +13,10 @@
     <main>
         <h1>Liens</h1>
 
-        <p class="descri">Une liste de liens vers des sites intéressants pour le web.</p>
+        <h2>Une liste de liens vers des sites intéressants pour le web</h2>
 
         <div class="liens">
-            <a href="https://www.awwwards.com/" target="blank">
+            <a href="https://www.awwwards.com/" target="_blank">
                 <div class="titre_mobile">
                     <h3>Awwwards</h3>
                     <img src="images/imgLiens/awwwards.png" alt="awwwards">
@@ -26,7 +26,7 @@
                 </div>
             </a>
 
-            <a href="https://www.codewars.com/" target="blank">
+            <a href="https://www.codewars.com/" target="_blank">
                 <div class="titre_mobile">
                     <h3>Codewars</h3>
                     <img src="images/imgLiens/codewars.png" alt="codewars">
@@ -36,7 +36,7 @@
                 </div>
             </a>
 
-            <a href="https://developer.mozilla.org/fr/" target="blank">
+            <a href="https://developer.mozilla.org/fr/" target="_blank">
                 <div class="titre_mobile">
                     <h3>Developer Mozilla</h3>
                     <img src="images/imgLiens/mozilla.png" alt="developer mozilla">
@@ -46,7 +46,7 @@
                 </div>
             </a>
 
-            <a href="https://stackoverflow.com/" target="blank">
+            <a href="https://stackoverflow.com/" target="_blank">
                 <div class="titre_mobile">
                     <h3 class="titre_mobile">Stackoverflow</h3>
                     <img src="images/imgLiens/overflow.png" alt="stack overflow">
