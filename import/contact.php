95bc54e96026e9b0fb910cf5e30d6c612e98275b
vicfir <vicfir@hotmail.fr>
2021-11-18 12:09:56 +0100

    contact html + css mobile

--- import/contact.php
+++ import/contact.php
@@ -12,6 +12,13 @@
 
     <main>
         <h1>Contact</h1>
+
+        <form action="" name="contact" method="POST">
+            <input type="text" name="thename" placeholder="Votre nom" required /><br>
+            <input type="text" name="themail" placeholder="Votre adresse mail" required /><br>
+            <textarea name="thetext" placeholder="Votre texte" required></textarea><br>
+            <input type="submit" value="Envoyer votre message"/>
+        </form>
     </main>
 
     <img class="burger" src="./images/burger.png" alt="menu" onclick="toggle();">
