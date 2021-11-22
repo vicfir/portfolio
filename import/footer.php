6e3d85b97b34062383a3bd565ece61bcdaac88e2
vicfir <vicfir@hotmail.fr>
2021-11-22 10:11:15 +0100

    footer

--- import/footer.php
+++ import/footer.php
@@ -1,3 +1,8 @@
 <footer>
+        <div class="logogitin">
+            <a href="https://github.com/vicfir" target="_blank"><img src="./images/github.png" alt=""></a>
+            <a href="https://github.com/vicfir" target="_blank"><img src="./images/linkedin.png" alt=""></a>
+        </div>
+
     <p>© 2021 Copyright: Victor Firkowski</p>
 </footer>
